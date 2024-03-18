6259cc943c722d59f1ee09dcc95d1b3c54bb751d
tracer-ysyx <tracer@ysyx.org>
2024-03-18 15:59:08 +0800

    > compile NEMU ysyx_23060124 xuzhi Linux Chelly 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:59:08 up  4:30,  0 users,  load average: 0.71, 0.31, 0.11

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,8 +5,8 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uint64_t lo = inl(RTC_ADDR + 4);
-  uint64_t hi = inl(RTC_ADDR);
+  uint64_t lo = inl(RTC_ADDR);
+  uint64_t hi = inl(RTC_ADDR + 4);
   uptime->us = (hi << 32) + lo;
 }
 
@@ -18,3 +18,27 @@ void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->month  = 0;
   rtc->year   = 1900;
 }
+
+// static uint64_t boot_time;
+
+// uint64_t getitimer(){
+//   uint32_t low = inl(RTC_ADDR);
+//   uint32_t high = inl(RTC_ADDR + 4);
+//   return ((uint64_t)low + (((uint64_t)high) << 32));
+// }
+// void __am_timer_init() {
+//   boot_time = getitimer();
+// }
+
+// void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
+//   uptime->us = getitimer() - boot_time;
+// }
+
+// void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
+//   rtc->second = 0;
+//   rtc->minute = 0;
+//   rtc->hour   = 0;
+//   rtc->day    = 0;
+//   rtc->month  = 0;
+//   rtc->year   = 1900;
+// }
