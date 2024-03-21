c4ec0d506296dada3c6e46738af923cef7d981b9
tracer-ysyx <tracer@ysyx.org>
2024-03-21 21:18:25 +0800

    > compile NEMU ysyx_23060124 xuzhi Linux Chelly 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:18:25 up  1:28,  2 users,  load average: 0.01, 0.03, 0.05

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,8 +5,8 @@
 
 void __am_gpu_init() {
   int i;
-  int w = 600;  // TODO: get the correct width
-  int h = 200;  // TODO: get the correct height
+  int w = 400;  // TODO: get the correct width
+  int h = 300;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
@@ -24,6 +24,16 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  uint32_t *pixels = ctl->pixels;
+  if (!ctl->sync && (w == 0 || h == 0)) return;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t W = inl(VGACTL_ADDR) >> 16;
+  for (int i = y; i < y + h; i++) {
+    for (int j = x; j < x + w; j++) {
+      fb[W*i+j] = pixels[w*(i-y)+(j-x)];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
